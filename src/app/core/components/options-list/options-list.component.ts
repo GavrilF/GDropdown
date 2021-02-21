0c9e3fb8bce007f3b94621cd14631334389768e4
GavrilF <fachikovdesign@gmail.com>
2021-02-21 22:04:49 +0200

    Add option list component

---
+++ src/app/core/components/options-list/options-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-options-list',
+  templateUrl: './options-list.component.html',
+  styleUrls: ['./options-list.component.scss']
+})
+export class OptionsListComponent implements OnInit {
+
+  @Input() options: string[];
+  @Input() selected: string;
+
+  @Output() optionChange: EventEmitter<string> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  isSelected(option: string){
+    return option === this.selected;
+  }
+
+  onOptionSelect(option:string){
+    this.optionChange.emit(option);
+  }
+
+}
