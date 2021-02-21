d02bdb6a3ee967d4ee3b82c048d82f534835964c
GavrilF <fachikovdesign@gmail.com>
2021-02-21 11:41:05 +0200

    Create rest of the base components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DropdownComponent } from './dropdown/dropdown.component';
+import { PopupDirective } from './directives/popup.directive';
+import { PopupContainerComponent } from './components/popup-container/popup-container.component';
+import { InputFieldComponent } from './components/input-field/input-field.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 
 
 
 @NgModule({
-  declarations: [DropdownComponent],
+  declarations: [DropdownComponent, PopupDirective, PopupContainerComponent, InputFieldComponent],
   imports: [
     CommonModule
   ],
