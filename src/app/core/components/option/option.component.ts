3023ddb357b6f9f20c729bc7c15e8d039411a4f9
GavrilF <fachikovdesign@gmail.com>
2021-02-21 22:04:25 +0200

    Add single option component

---
+++ src/app/core/components/option/option.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-option',
+  templateUrl: './option.component.html',
+  styleUrls: ['./option.component.scss']
+})
+export class OptionComponent implements OnInit {
+
+  @Input() selected: boolean;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
