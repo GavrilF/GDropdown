c277f9a8ed62995660dee18a46413a7f6eaf6e9d
GavrilF <fachikovdesign@gmail.com>
2021-02-23 19:58:43 +0200

    Options list unit tests

--- src/app/core/components/dropdown/dropdown.component.spec.ts
+++ src/app/core/components/dropdown/dropdown.component.spec.ts
@@ -1,14 +1,45 @@
+import { Component, Directive, Input, TemplateRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DropdownComponent } from './dropdown.component';
 
 describe('DropdownComponent', () => {
+  //Fake InputField
+  @Component({
+    selector: 'app-input-field',
+    template: '<div><div>'
+  })
+  class MockInputFieldComponent {
+    @Input() ngModel: any
+  }
+  // Fake OptionsList
+  @Component({
+    selector: 'app-options-list',
+    template: '<div><div>'
+  })
+  class MockOptionsListComponent {
+    @Input() options: string[];
+    @Input() selected: string;
+    @Input() focused: string;
+  }
+  // Fake PopupDirective
+  @Directive({
+    selector: '[appPopup]'
+  })
+  class PopupDirective {
+    @Input() templateRef: TemplateRef<HTMLElement>;
+    @Input() adaptToElementWidth: boolean;
+    @Input() useBoolToOpen: boolean;
+    @Input() isOpen: boolean; 
+  }
+
   let component: DropdownComponent;
   let fixture: ComponentFixture<DropdownComponent>;
+  let OPTIONS: string[];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DropdownComponent ]
+      declarations: [ DropdownComponent, MockInputFieldComponent, MockOptionsListComponent]
     })
     .compileComponents();
   });
@@ -16,10 +47,24 @@ describe('DropdownComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DropdownComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    OPTIONS = ['First', 'Second', 'Third', 'Fourth'];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set selectedOption properly', () => {
+    component.selectedOption = 'First';
+
+    expect(component.selectedOption).toBe('First');
+  });
+
+  it('should set inputField value when setting selectedOption', () => {
+    component.selectedOption = 'First';
+
+    expect(component.inputValue).toBe('First');
+  });
+
+  
 });
--- src/app/core/components/options-list/options-list.component.spec.ts
+++ src/app/core/components/options-list/options-list.component.spec.ts
@@ -1,14 +1,18 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { OptionComponent } from '../option/option.component';
 
 import { OptionsListComponent } from './options-list.component';
 
 describe('OptionsListComponent', () => {
   let component: OptionsListComponent;
   let fixture: ComponentFixture<OptionsListComponent>;
+  let OPTIONS: string[];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ OptionsListComponent ]
+      declarations: [ OptionsListComponent, OptionComponent ]
     })
     .compileComponents();
   });
@@ -16,10 +20,45 @@ describe('OptionsListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(OptionsListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    OPTIONS = ['First', 'Second', 'Third', 'Fourth'];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render each option as OptionComponent', () => {
+    component.options = OPTIONS;
+    fixture.detectChanges();
+
+    const optionComponentsDEs = fixture.debugElement.queryAll(By.directive(OptionComponent));
+    expect(optionComponentsDEs.length).toEqual(4);
+  })
+
+  it('should get isSelected', () => {
+    component.options = OPTIONS;
+    component.selected = OPTIONS[0];
+    fixture.detectChanges();
+
+    expect(component.isSelected(OPTIONS[0])).toBeTrue();
+  })
+
+  it('should get isFocused', () => {
+    component.options = OPTIONS;
+    component.focused = OPTIONS[0];
+    fixture.detectChanges();
+
+    expect(component.isFocused(OPTIONS[0])).toBeTrue();
+  });
+
+  it('should emit selectedOption', () => {
+    spyOn(component.optionChange, 'emit');
+    component.options = OPTIONS;
+    fixture.detectChanges();
+
+    const firstOptionComponent= fixture.debugElement.queryAll(By.directive(OptionComponent))[0];
+    firstOptionComponent.triggerEventHandler('click', {});
+
+    expect(component.optionChange.emit).toHaveBeenCalledWith(OPTIONS[0]);
+  })
 });
